f8d57cf77c65060a495921108be5b27dd5dfacde
haruki1125 <haruki1125@users.noreply.github.com>
2020-12-10 17:49:46 +0900

    commit2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def new
-    @user = User.new
-  end
-
-  def create
-  end
-
-  def destroy
-    tweet = User.find(params[:id])
-    tweet.destroy
-  end
+  
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,30 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true,
-                        format: {
-                          with: /\A[ぁ-んァ-ヶ一-龥々]+\z/,
-                          message: '全角（漢字・ひらがな・カタカナ）のみで入力して下さい'
-                        }
 
-  validates :first_name, presence: true,
-                         format: {
-                           with: /\A[ぁ-んァ-ヶ一-龥々]+\z/,
-                           message: '全角（漢字・ひらがな・カタカナ）のみで入力して下さい'
-                         }
 
-  validates :last_name_kana, presence: true,
-                             format: {
-                               with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
-                               message: '全角カタカナのみで入力して下さい'
-                             }
-  validates :first_name_kana, presence: true,
-                              format: {
-                                with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
-                                message: '全角カタカナのみで入力して下さい'
-                              }
-  validates :birthday, presence: true
+        with_options presence: true do
+          validates :nickname
+          validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/,message: '全角（漢字・ひらがな・カタカナ）のみで入力して下さい'}
+          validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/,message: '全角（漢字・ひらがな・カタカナ）のみで入力して下さい'}
+          validates :last_name_kana, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: '全角カタカナのみで入力して下さい'}
+          validates :first_name_kana, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: '全角カタカナのみで入力して下さい'}
+          validates :birthday
+        end
+        
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   # 半角英数字のみ
