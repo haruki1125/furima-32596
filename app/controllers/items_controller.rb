84903dcb311cbf84845bbca7cc3a7dba744c05cd
haruki1125 <haruki1125@users.noreply.github.com>
2020-12-15 12:44:28 +0900

    commit3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:destroy, :show]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -21,13 +21,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == item.user_id
-      item.destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
     else
       render :show
@@ -40,5 +38,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_condition_id, :item_fee_status_id, :item_prefecture_id, :item_scheduled_id, :item_price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
